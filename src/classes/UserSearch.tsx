14e0ff501b962d1e479f51d19d8e4e1deb1699e5
Simone Taeggi <startae14@gmail.com>
2023-05-05 14:48:39 +0200

    define Types in a Class Component

---
+++ src/classes/UserSearch.tsx
@@ -0,0 +1,64 @@
+import { Component } from "react";
+
+const users = [
+  {
+    name: 'Sarah',
+    age: 20,
+  },
+  {
+    name: 'Alex',
+    age: 20,
+  },
+  {
+    name: 'Michael',
+    age: 20,
+  },
+]
+
+interface User {
+  name: string;
+  age: number;
+}
+
+interface UserSearchProps {
+  users: User[]
+}
+
+interface UserSearchState {
+  name: string;
+  user: User | undefined;
+}
+
+class UserSearch extends Component<UserSearchProps> {
+  state: UserSearchState = {
+    name: '',
+    user: undefined
+  };
+
+  onClick = () => {
+    const foundUser = this.props.users.find(user => user.name === this.state.name);
+    this.setState({
+      user: foundUser
+    });
+  }
+  render() {
+    const {
+      name,
+      user,
+    } = this.state;
+    return <div>
+      User Search
+      <input value={name} onChange={e => this.setState({
+        name: e.target.value
+      })} />
+      <button onClick={this.onClick}>Find User</button>
+      <div>
+        { user && user.name }
+        { user && user.age }
+      </div>
+    </div>;
+  }
+  
+};
+
+export default UserSearch;
