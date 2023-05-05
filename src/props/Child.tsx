a274e06dd6fc9707e2d026f2ef70d75f15e46ab2
Simone Taeggi <startae14@gmail.com>
2023-05-05 12:19:56 +0200

    define Functional Component Annotation

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -5,3 +5,11 @@ interface ChildProps {
 export const Child = ({ color }: ChildProps) => {
   return <div>{color}</div>
 };
+
+export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
+  return <div>{color}</div>;
+};
+
+export const ChildAsFunctionComponent: React.FunctionComponent<ChildProps> = ({ color }) => {
+  return <div>{color}</div>;
+};
