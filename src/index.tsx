111dc5803045c959d416b6c0d44d21cdc08962c8
Simone Taeggi <startae14@gmail.com>
2023-05-05 13:12:30 +0200

    define the state as Union of types

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import { createRoot } from "react-dom/client";
-import GuestList from "./state/GuestList";
+import UserSearch from "./state/UserSearch";
 
 const App = () => {
   return <div>
-    <GuestList />
+    <UserSearch />
   </div>
 };
 
