f32ec04f5314cbdcc27d3671cf51f12632d361d1
Simone Taeggi <startae14@gmail.com>
2023-05-05 15:12:28 +0200

    define the Type of a Ref on a input element

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import { createRoot } from "react-dom/client";
-import EventComponent from "./events/EventComponent";
+import UserSearch from "./refs/UserSearch";
 
 const App = () => {
   return <div>
-    <EventComponent />
+    <UserSearch />
   </div>
 };
 
