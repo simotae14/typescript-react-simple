b2702c44b1484b443c68bc80250d64ccedf0ea44
Simone Taeggi <startae14@gmail.com>
2023-05-05 14:25:30 +0200

    define the Type of Event change on a input element

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import { createRoot } from "react-dom/client";
-import UserSearch from "./state/UserSearch";
+import EventComponent from "./events/EventComponent";
 
 const App = () => {
   return <div>
-    <UserSearch />
+    <EventComponent />
   </div>
 };
 
